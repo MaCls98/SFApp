78c9bdf0059c45df01f762091786ca43a4ce0d5e
MaCls98 <manuel.celis@uptc.edu.co>
2019-07-23 05:28:18 -0500

    :c

--- app/src/main/java/com/android/sfapp/MainActivity.java
+++ app/src/main/java/com/android/sfapp/MainActivity.java
@@ -741,7 +741,6 @@ public class MainActivity extends AppCompatActivity {
                         }
                         isRequest = true;
                 } catch (JSONException e) {
-
                         e.printStackTrace();
                 }
             }
